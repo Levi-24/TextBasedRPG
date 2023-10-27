68f7eef21072703fe0ed2df592e05c3cee38adb9
Levi-24 <leventemajor36@gmail.com>
2023-10-27 23:49:48 +0200

    Adde Character Class

---
+++ TextBasedRPG/Character.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedRPG
+{
+    internal class Character
+    {
+        // Character Info
+        public string Name { get; set; }
+        public bool Class { get; set; }
+        //true = Knight
+        //false = Mage
+        public int Level { get; set; }
+        public int Experience { get; set; }
+        public int Health { get; set; }
+        public int Mana { get; set; }
+
+        // Stats
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Intelligence { get; set; }
+        public int Speed { get; set; }
+        public int Luck { get; set; }
+
+        // Combat Stats
+        public int Defense { get; set; }
+        public int MagicDefense { get; set; }
+
+        public int PhysicalDamage { get; set; }
+        public int MagicDamage { get; set; }
+
+
+
+
+        public int Accuracy { get; set; }
+        public int Evasion { get; set; }
+
+        public int CriticalChance { get; set; }
+        public int CriticalDamage { get; set; }
+
+        public int AttackSpeed { get; set; }
+
+        // Inventory
+        public int CarryAmount { get; set; }
+        public int Gold { get; set; }
+
+        // Skills
+        public int SkillPoints { get; set; }
+        public int StatPoints { get; set; }
+        public string[] Skills { get; set; }
+    }
+}
--- TextBasedRPG/Program.cs
+++ TextBasedRPG/Program.cs
@@ -8,25 +8,67 @@ namespace TextBasedRPG
         {
             DisplayMethods.Program DisplayMethods = new DisplayMethods.Program();
             DisplayMethods.MainScreen();
-            ConsoleKey titleScreenSelect = Console.ReadKey().Key;
-            switch (titleScreenSelect)
-            {
-                case ConsoleKey.D1:
-                    Console.Clear();
-                    Console.WriteLine("You chose to start a new game.");
-                    break;
-                case ConsoleKey.D2:
-                    Console.Clear();
-                    Console.WriteLine("You chose to load a game.");
-                    break;
-                case ConsoleKey.D3:
-                    Console.WriteLine("You chose to exit.");
-                    break;
-                default:
-                    break;
-            }
 
-            Console.ReadKey();
+            bool exit = false;
+            while (!exit)
+            {
+                ConsoleKey titleScreenSelect = Console.ReadKey().Key;
+                switch (titleScreenSelect)
+                {
+                    //New Game
+                    case ConsoleKey.D1:
+                        Console.Clear();
+                        Console.WriteLine("You chose to start a new game.");
+
+                        Character character = new Character();
+                        Console.Write("Choose a name: ");
+                        character.Name = Console.ReadLine();
+
+                        Console.WriteLine("Choose a class:");
+                        Console.WriteLine("\tKnight = K");
+                        Console.WriteLine("\tMage = M");
+                        ConsoleKey classSelect = Console.ReadKey().Key;
+                        do
+                        {
+                            
+                            switch (classSelect)
+                            {
+                                //Knight class selected
+                                case ConsoleKey.K:
+                                    character.Class = true;
+                                    break;
+                                //Mage class selected
+                                case ConsoleKey.M:
+                                    character.Class = false;
+                                    break;
+                                //Invalid class selection
+                                default:
+                                    Console.WriteLine("Please choose a valid class.");
+                                    classSelect = Console.ReadKey().Key;
+                                    break;
+                            }
+                        } while (classSelect != ConsoleKey.K || classSelect != ConsoleKey.M);
+
+                        Console.WriteLine("--------------");
+                        Console.WriteLine(character.Name);
+                        Console.WriteLine(character.Class);
+                        break;
+                        //Load Game
+                    case ConsoleKey.D2:
+                        Console.Clear();
+                        Console.WriteLine("You choose to load your game.");
+                        break;
+                        //Exit
+                    case ConsoleKey.D3:
+                        exit = true;
+                        break;
+                    default:
+                        exit = true;
+                        break;
+                }
+
+         
+            }
         }
     }
 }
