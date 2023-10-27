6bcb7aa1e2ed996d768c24e6973e68fffb66acd2
Levi-24 <leventemajor36@gmail.com>
2023-10-27 19:02:39 +0200

    Add project files.

---
+++ TextBasedRPG/DisplayMethods.cs
@@ -0,0 +1,27 @@
+﻿namespace DisplayMethods
+{
+    internal class Program
+    {
+        public void MainScreen()
+        {
+            Console.WriteLine("XxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXx");
+            Console.WriteLine(@"
+                                                                       
+                                _-_ _,,   ,,                ,, ,,                 |\   
+                                   -/  )  ||                || ||      _           \\  
+                                  ~||_<   ||  /'\\  /'\\  /-|| ||/|,  < \, \\/\\  / \\ 
+                                   || \\  || || || || || || || || ||  /-|| || || || || 
+                                   ,/--|| || || || || || || || || |' (( || || || || || 
+                                  _--_-'  \\ \\,/  \\,/   \\/  \\/    \/\\ \\ \\  \\/  
+                                 (                                                     
+
+                              ");
+            Console.WriteLine("XxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXx");
+            Console.WriteLine("This is a basic text based RPG made for fun.");
+            Console.WriteLine("Please choose from the options below:");
+            Console.WriteLine("\t1. Start New Game");
+            Console.WriteLine("\t2. Load Game");
+            Console.WriteLine("\t3. Exit");
+        }
+    }
+}
---
+++ TextBasedRPG/Program.cs
@@ -0,0 +1,32 @@
+﻿using DisplayMethods;
+
+namespace TextBasedRPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayMethods.Program DisplayMethods = new DisplayMethods.Program();
+            DisplayMethods.MainScreen();
+            ConsoleKey titleScreenSelect = Console.ReadKey().Key;
+            switch (titleScreenSelect)
+            {
+                case ConsoleKey.D1:
+                    Console.Clear();
+                    Console.WriteLine("You chose to start a new game.");
+                    break;
+                case ConsoleKey.D2:
+                    Console.Clear();
+                    Console.WriteLine("You chose to load a game.");
+                    break;
+                case ConsoleKey.D3:
+                    Console.WriteLine("You chose to exit.");
+                    break;
+                default:
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
